15ecf60721ac2b2d07238943e2697324647f406e
Alexandre Lebas <58038165+alebas-fr@users.noreply.github.com>
2021-09-14 11:10:07 +0200

    Ajout de la question 1 + modif graphique python

--- TP1/TP1MSI.py
+++ TP1/TP1MSI.py
@@ -20,13 +20,49 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import random
 import datetime
+import threading
+import time
+import math
 get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# # Question 1 :
+# <p> Combien faut-il acheter de boîtes de céréales en moyenne
+# pour avoir la collection complète des joueurs ? (on, suppose bien
+# entendu que l’on a la même probabilité d’obtenir chacune des
+# images dans chaque paquet de céréales</p>
+# 
+# <p> <a href="https://www.ilemaths.net/sujet-le-collectionneur-469931.html">Lien</a> pour aider à la démonstration de la formule a utilisé. </p>
+
+# In[2]:
+
+
+def moyenneObtenirTouteLesCartes(n):
+    """
+    Fonction qui calcule combien il faut acheter de boîtes en moyenne pour obtenir toutes les cartes.
+    n représente le nombre de cartes qu'il y a obtenir
+    k représente le nombre de cartes que l'on possède 
+    """
+    k=1
+    somme1k = 0
+    moyenne = 0 # Nombre de boite que l'on doit acheter en moyenne
+    while k<=n:
+        somme1k+=(1/k)
+        k+=1
+    moyenne = n*somme1k
+    # Une fois que l'on possède le nombre moyen de boîtes qu'il faut acheter il faut arrondir par excès.
+    # Car la moyenne et un nombre réel et que le nombre de boîtes est entier.
+    # Renvoie le nombre de boîtes qu'il faut acheter en moyenne
+    return math.ceil(moyenne)
+print("Le nombre de boîtes qu'il faut acheter en moyenne pour avoir toutes les cartes est ",
+      moyenneObtenirTouteLesCartes(11))
+    
 
 
 # # Question 2 : 
 # <p> Développer une fonction qui réalise N tests. </p>
 
-# In[2]:
+# In[3]:
 
 
 # Pour simplifier on ne donne pas de nom aux cartes mais on leur donne un numéro entre 0 et 10
@@ -35,10 +71,10 @@ def Ntests(N):
     return tirage
 
 
-# In[3]:
+# In[4]:
 
 
-N = 30
+N = 50
 experience = Ntests(N)
 print(experience)
 
@@ -47,7 +83,7 @@ print(experience)
 # <p>Réaliser un frame composé de la date de test et du nombre
 # de tirage.</p>
 
-# In[4]:
+# In[5]:
 
 
 date = datetime.datetime.now()
@@ -55,7 +91,7 @@ df1 = pd.DataFrame(data={'Date':[date],'Nombre de tirage':[N]})
 print(df1)
 
 
-# In[5]:
+# In[6]:
 
 
 def dataframeDateEtTest(N):
@@ -73,38 +109,38 @@ def dataframeDateEtTest(N):
 # # Question 4 : 
 # <p>Afficher la structure du frame.</p>
 
-# In[6]:
+# In[7]:
 
 
-frame = dataframeDateEtTest(30)
+frame = dataframeDateEtTest(50)
 print(frame)
 
 
 # # Question 5 : 
 # <p> Afficher la courbe des tirages.</p>
 
-# In[7]:
+# In[8]:
 
 
 NombreDeValeur = frame['Tirage'].value_counts()
 print(NombreDeValeur)
 
 
-# In[8]:
+# In[9]:
 
 
 NombreDeValeur = NombreDeValeur.sort_index()
 print(NombreDeValeur)
 
 
-# In[9]:
+# In[10]:
 
 
-X = list(range(0,NombreDeValeur.shape[0]))
+X = list(range(1,NombreDeValeur.shape[0]+1))
 X
 
 
-# In[10]:
+# In[11]:
 
 
 plt.figure(figsize=(10,10))
@@ -115,14 +151,8 @@ plt.show() # Tracer
 # # Question 6 : 
 # <p>  Exporter le frame dans un fichier excel</p>
 
-# In[11]:
+# In[12]:
 
 
 frame.to_excel("TP1_py.xlsx")
-
-
-# In[ ]:
-
-
-
 
--- TP1/TP1MSI.r
+++ TP1/TP1MSI.r
@@ -1,12 +1,32 @@
 # Faire un premier tirage 
-# pour simplifier on ne donne pas de nom aux cartes mais on leur donne un numéro entre 1 et 11
+# Pour simplifier on ne donne pas de nom aux cartes mais on leur donne un numéro entre 1 et 11
 tirage = sample(x=1:11,size=20,replace=T)
 print(tirage)
 
 mode(tirage)
 
+moyenneObtenirTouteLesCartes = function(n){
+    # Fonction qui calcule combien il faut acheter de boîtes en moyenne pour obtenir toutes les cartes.
+    # n représente le nombre de cartes qu'il y a obtenir
+    # k représente le nombre de cartes que l'on possède 
+    k = 1
+    somme1k = 0
+    moyenne = 0 # Nombre de boite que l'on doit acheter en moyenne
+    while (k<=n){
+        somme1k = somme1k + (1/k)
+        k = k+1
+    }
+    moyenne = n*somme1k
+    # Une fois que l'on possède le nombre moyen de boîtes qu'il faut acheter il faut arrondir par excès.
+    # Car la moyenne et un nombre réel et que le nombre de boîtes est entier.
+    moyenne = ceiling(moyenne)
+    # Renvoie le nombre de boîtes qu'il faut acheter en moyenne
+    return(moyenne)
+}
+moyenneObtenirTouteLesCartes(11)
+
 # Définir le nombre de tirages que l'on souhaite réaliser
-nombreDeTirage = 30
+nombreDeTirage = 40
 
 NTest <- function(N){
     # Fonction qui réalise les N tirages aléatoires de carte 
@@ -20,13 +40,29 @@ experience = as.numeric(NTest(nombreDeTirage))
 print(experience)
 
 # On récupère la date 
-Date = Sys.Date()
+Date = Sys.time()
 print(Date)
 
 # On crée le frame avec la date et le nombre de tirages
 (frame = data.frame(Date,nombreDeTirage))
 
+dataframeDateEtTest = function(N){
+    i=1
+    date = rep(Sys.time(),N)
+    tirages = rep(0,N)
+    while (i<=N){
+        tirages[i]=sample(x=1:11,size=1)
+        date[i]=Sys.time()
+        i=i+1
+    }
+    (df<- data.frame(date,tirages))
+    return(df)
+}
+frame1 = dataframeDateEtTest(30)
+
 print(frame)
+
+print(frame1)
 
 compterValeur = function(tirage){
     # Cette fonction permet de compter combien de fois nous avons chaque carte
@@ -49,4 +85,4 @@ install.packages("writexl")
 
 # Sauvegarder le frame dans un fichier excel nommé TP1.xlsx
 library("writexl")
-write_xlsx(frame, "TP1.xlsx")
+write_xlsx(frame1, "TP1.xlsx")
